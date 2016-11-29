41763077f5a910d9cbf61e77fd291e2f0ff0dd56
Gustav Malm <gustav.malm94@hotmail.com>
2016-11-29 14:11:38 +0100

    Added the feature to calculate if the player gets a bonus or not (#3)

--- src/Controller/Yatzy.java
+++ src/Controller/Yatzy.java
@@ -3,9 +3,24 @@ package Controller;
 import javax.swing.*;
 
 public class Yatzy {
-    private JFrame window;
+	private JFrame window;
     private int rollsLeft;
     private ScoreBoard scoreBoard;
     private ArrayList<Player> players = new ArrayList<Player>();
     private ArrayList<Dice> dice = new ArrayList<Dice>();
+	
+	public int getBonus(Player player) {
+		
+		int bonus = 0;
+		
+		if(this.getUpperSectionScore(player) >= 63) {
+			
+			bonus = 50;
+			
+		}
+		
+		return bonus;
+		
+	}
+
 }
