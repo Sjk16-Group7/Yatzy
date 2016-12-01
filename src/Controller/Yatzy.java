f2e916132d123e85d8e80f2832d2b7af4c0e89de
Marianna Kachadurian <mariannakhachadourian@gmail.com>
2016-12-01 13:04:19 +0100

    added getUpperSectionScore method

--- src/Controller/Yatzy.java
+++ src/Controller/Yatzy.java
@@ -1,26 +1,44 @@
 package Controller;
 
+import java.util.ArrayList;
 import javax.swing.*;
+
+import Model.Player;
+import Model.ScoreBoard;
+import Model.ScoreBoardCell;
+import Model.Dice;
 
 public class Yatzy {
 	private JFrame window;
-    private int rollsLeft;
-    private ScoreBoard scoreBoard;
-    private ArrayList<Player> players = new ArrayList<Player>();
-    private ArrayList<Dice> dice = new ArrayList<Dice>();
-	
+	private int rollsLeft;
+	private ScoreBoard scoreBoard;
+	private ArrayList<Player> players = new ArrayList<Player>();
+	private ArrayList<Dice> dice = new ArrayList<Dice>();
+
 	public int getBonus(Player player) {
-		
 		int bonus = 0;
-		
-		if(this.getUpperSectionScore(player) >= 63) {
-			
+
+		if (this.getUpperSectionScore(player) >= 63) {
 			bonus = 50;
-			
 		}
-		
+
 		return bonus;
-		
 	}
 
+	public int getUpperSectionScore(Player player) {
+		int sum = 0;
+		int numberOfUpperIndex = 6;
+		int indexOfPlayer = this.players.indexOf(player);
+
+		for (int i = 0; i < numberOfUpperIndex; i++) {
+			ScoreBoardCell cell = this.scoreBoard.getCell(i, indexOfPlayer);
+			String value = cell.getValue();
+
+			if (!cell.isCrossed()) {
+				sum += Integer.parseInt(value);
+			}
+		}
+
+		return sum;
+	}
 }
